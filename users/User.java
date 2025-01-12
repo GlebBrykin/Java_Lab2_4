ca5b9ae2452e0324dd83cadeea464bf813abbb98
GlebBrykin <glebsbrykin@gmail.com>
2025-01-12 19:27:11 +0300

    Add files via upload

--- users/User.java
+++ users/User.java
@@ -1,1 +1,18 @@
+package users;
 
+import library.Book;
+
+public class User
+{
+    public String name;
+
+    public User(String name)
+    {
+        this.name = name;
+    }
+
+    public void takeBook(Book book)
+    {
+        book.checkOut(this.name);
+    }
+}
